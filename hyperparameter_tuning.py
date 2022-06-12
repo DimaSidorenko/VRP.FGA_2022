7f47ef6be5d59a94e3fc80f458c9fd73fb5e63b4
Сидоренко Дима <dima.sidorenk@yandex.ru>
2022-06-12 11:39:41 +0500

    research results

--- hyperparameter_tuning.py
+++ hyperparameter_tuning.py
@@ -78,6 +78,7 @@ def make_research(test_path, vertexCount):
     dr_exponent_value = get_exponent_pow(1 / vertexCount) - 2
     dr_left_border_digit = 1
     dr_right_border_digit = 20
+    # dr_right_border_digit = 4
     dr_step_digit = 2
 
     ilr_exponent_value = dr_exponent_value + 1
@@ -85,6 +86,7 @@ def make_research(test_path, vertexCount):
 
     ilr_left_border_digit = 15
     ilr_right_border_digit = 80
+    # ilr_right_border_digit = 20
     ilr_step_digit = 4
 
     for ilr_digit in range(ilr_left_border_digit, ilr_right_border_digit, ilr_step_digit):
@@ -102,32 +104,49 @@ def make_research(test_path, vertexCount):
 def norm2(value, min, max):
     return (value) / (max)
 
+
+def get_median(list):
+    list.sort()
+    return list[1 : len(list) - 1]
+
+# returns two lists
+# list1[i] = [grouped_metrica_value_for_current_iLR, current_iLR]
+# list2[i] = [sorted_DR_values, current_iLR]
+# необходимо еще в самом начале сделать некоторый препроцессинг
 def group_results(results):
     if (len(results) == 0):
         return []
 
-    grouped_result = np.zeros(len(results[0]))
+    results = np.array(results)
 
+    x = results[0][:, 1]
+
+    for idx, result in enumerate(results):
+        mn = np.min(results[idx])
+        mx = np.max(results[idx])
+
+        for i in range(len(result)):
+            result[i][0] = norm2(result[i][0], mn, mx)
+
+    # print(results)
+
+    slice_metrica_DR = [[] for i in range(len(x))]
     for result in results:
-        result = np.array(result)
-        temp = result[:, 0]
+        for idx, values in enumerate(result):
+            # print(values)
+            slice_metrica_DR[idx].append([values[0], values[2]])
 
-        mx = np.max(temp)
-        mn = np.min(temp)
+    grouped_metrica_result = np.zeros(len(x))
 
-        normilized_metric_values = [norm2(elem, mn, mx) for elem in temp]
-        grouped_result += np.array(normilized_metric_values)
+    for idx in range(len(slice_metrica_DR)):
+        slice_metrica_DR[idx] = get_median(slice_metrica_DR[idx])
 
-    result = results[0];
-    x = np.array(result)[:, 1]
+        temp = np.array(slice_metrica_DR[idx])
+        grouped_metrica_result[idx] = np.sum(temp[:, 0])
 
-    answer = []
-    for i in range(len(x)):
-        answer.append([grouped_result[i], x[i]])
+    final_results = [[grouped_metrica_result[idx], x[idx]] for idx in range(len(x))]
 
-    # print(answer)
-
-    return answer
+    return final_results
 
 
 def main():
@@ -137,7 +156,7 @@ def main():
     for vertex_folder_path in vertex_folder_pathes:
         vertex_count = int(vertex_folder_path.split('\\')[-1])
 
-        if (vertex_count != 30):
+        if (vertex_count == 30):
             continue
 
         # print(sz)
@@ -145,21 +164,25 @@ def main():
                                os.listdir(vertex_folder_path)]
         # print(current_test_pathes)
 
-        vertex_results = []
+        vertex_results_special = []
+        vertex_results_random = []
 
         for test_path in current_test_pathes:
             research_result = make_research(test_path, vertex_count)
             
-            vertex_results.append(research_result)
+            test_name = test_path.split('\\')[-1]
 
             test_filename = test_path.split('\\')[-1]
             last_point_pos = test_filename.rindex('.')
             test_filename = test_filename[0:last_point_pos]
 
-            file_path_template = os.path.join(RESULTS_DIR, str(vertex_count), test_filename)
+            if (test_name.find('special') != -1):
+                 vertex_results_special.append(research_result)   
+            else:
+                 vertex_results_random.append(research_result)   
+ 
             
-            # print(file_path_template)
-
+            file_path_template = os.path.join(RESULTS_DIR, str(vertex_count), test_filename)
             # write results in txt file
             txt_file_path = file_path_template + '_results.txt'
 
@@ -174,19 +197,18 @@ def main():
             draw_data(research_result, plot_file_path, vertex_count)
         
         
-        # plot_file_path = file_path_template + '_plot.png'
-        # draw plots
-        # draw_data(research_result, plot_file_path, vertex_count)
-
-
-        grouped_data = group_results(vertex_results)        
-        
+        #for_special_tests
+        grouped_data_special = group_results(vertex_results_special)        
         file_path_template = os.path.join(RESULTS_DIR, str(vertex_count), str(vertex_count))
-        plot_file_path = file_path_template + '_plot.png'
+        plot_file_path = file_path_template + '_special_plot.png'
+        draw_data(grouped_data_special, plot_file_path, vertex_count)
 
-        draw_data(grouped_data, plot_file_path, vertex_count)
+        #for_random_tests
+        grouped_data_random = group_results(vertex_results_random)        
+        file_path_template = os.path.join(RESULTS_DIR, str(vertex_count), str(vertex_count))
+        plot_file_path = file_path_template + '_random_plot.png'
+        draw_data(grouped_data_random, plot_file_path, vertex_count)
 
-        break
 
 if __name__ == "__main__":
     main()
